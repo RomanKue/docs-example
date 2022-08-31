3dc7457cd1d80dcc8e97ba374cac48e364f5dfcb
Daniel Strobusch (FG-410) <Daniel.Strobusch@bmw.de>
2022-08-31 14:48:06 +0200

    comment on issue

--- .github/actions/src/lib/octokit.ts
+++ .github/actions/src/lib/octokit.ts
@@ -2,6 +2,8 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 import {Issue} from './github/issue.js';
 import {Api} from '@octokit/plugin-rest-endpoint-methods/dist-types/types.js';
+import {IssueComment} from './github/issue-comment.js';
+import {issue} from '@actions/core/lib/command.js';
 
 const getGithubToken = (): string => {
   const githubToken = core.getInput('GITHUB_TOKEN');
@@ -11,15 +13,42 @@ const getGithubToken = (): string => {
   return githubToken;
 };
 
-export const getIssue = async (options: Partial<Parameters<Api["rest"]['issues']['get']>[0]> = {}) => {
+type RestApi = Api['rest'];
+type IssuesApi = RestApi['issues'];
+
+/**
+ * see https://docs.github.com/en/rest/issues/issues#get-an-issue
+ */
+export const getIssue = async (
+  options: Partial<Parameters<IssuesApi['get']>[0]> = {}
+): Promise<Issue> => {
   const octokit = github.getOctokit(getGithubToken());
-  let issue = (await octokit.rest.issues.get({
+  let response = await octokit.rest.issues.get({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       issue_number: github.context.issue.number,
       ...options
     }
-  )).data as Issue;
+  );
+  const issue = response.data as Issue;
   core.debug(`${JSON.stringify(issue, null, 2)}`);
   return issue;
+};
+
+/**
+ * see https://docs.github.com/en/rest/issues/comments#create-an-issue-comment
+ */
+export const commentOnIssue = async (
+  options: { body: string } & Partial<Parameters<IssuesApi['createComment']>[0]>
+): Promise<IssueComment> => {
+  const octokit = github.getOctokit(getGithubToken());
+  const response = await octokit.rest.issues.createComment({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.issue.number,
+    ...options
+  });
+  const issueComment = response.data as IssueComment;
+  core.debug(`${JSON.stringify(issueComment, null, 2)}`);
+  return issueComment;
 };
--- .github/actions/src/review-new-app-issue/main.ts
+++ .github/actions/src/review-new-app-issue/main.ts
@@ -5,18 +5,21 @@
  */
 import * as core from "@actions/core";
 import {NewAppIssue, parseIssueBody} from './new-issue.js';
-import {getIssue} from '../lib/octokit.js';
+import {commentOnIssue, getIssue} from '../lib/octokit.js';
+import {Issue} from '../lib/github/issue.js';
 
-
-const checkAppSchema = (newAppIssue: NewAppIssue) => {
+const checkAppSchema = (issue: Issue, newAppIssue: NewAppIssue) => {
 }
 
-const checkTermsOfService = (newAppIssue: NewAppIssue) => {
+const checkTermsOfService = (issue: Issue, newAppIssue: NewAppIssue) => {
+  core.info(`checking if terms of service are accepted`)
+  let userLogin = issue.user?.login;
   if (!newAppIssue.termsOfServiceAccepted) {
+    commentOnIssue({body: `@${userLogin} it seems that you did not agree to the terms of service yet. Could you please check and update your issue, so I can proceed with your request.`})
   }
 }
 
-const assignIssue = (newAppIssue: NewAppIssue) => {
+const assignIssue = (issue: Issue, newAppIssue: NewAppIssue) => {
 }
 
 
@@ -24,9 +27,9 @@ const run = async () => {
   const issue = await getIssue();
   const newAppIssue = parseIssueBody(issue.body ?? '')
 
-  checkTermsOfService(newAppIssue)
-  checkAppSchema(newAppIssue)
-  assignIssue(newAppIssue)
+  checkTermsOfService(issue, newAppIssue)
+  checkAppSchema(issue, newAppIssue)
+  assignIssue(issue, newAppIssue)
 }
 
 run().catch(e => {

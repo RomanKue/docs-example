4c1b855ec97acaecda1cb1ec077c4850087cf5ad
Daniel Strobusch (FG-410) <Daniel.Strobusch@bmw.de>
2022-09-07 14:30:58 +0200

    add createMakeStubWorkflow

--- actions/src/lib/unity/app-repo/index.ts
+++ actions/src/lib/unity/app-repo/index.ts
@@ -15,6 +15,48 @@ import {
 } from '../../github/api/repos/repositories.js';
 import {Repository} from '../../github/api/repos/response/repository.js';
 
+function createMakeStubWorkflow() {
+  return `
+name: make-stub
+description: make a stub app and add it to the repository
+on:
+  workflow_dispatch:
+    inputs:
+      name:
+        description: 'the name of the deployable'
+        required: true
+        type: string
+      type:
+        description: the type of stub to create
+        required: true
+        type: choice
+        options:
+          - angular
+          - quarkus
+      branch:
+        description: 'the branch where the app should be created'
+        required: true
+        default: 'main'
+        type: string
+jobs:
+  make-stub
+    permissions:
+      contents: write
+      id-token: write
+    runs-on: atc-ubuntu-latest
+    timeout-minutes: 15
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          ref: \${{ inputs.branch }}
+      - uses: unity/make-stub@v1
+        with:
+          name: \${{ inputs.name }}
+          type: \${{ inputs.type }}
+          branch: \${{ inputs.branch }}
+    `;
+}
+
 export const createRepository = async (appSpec: AppSpec): Promise<Repository> => {
   const newAppRepoName = repoName(appSpec.name);
   if (await repositoriesUtils.isRepoExistent(appSpec.name)) {
@@ -31,29 +73,22 @@ export const createRepository = async (appSpec: AppSpec): Promise<Repository> =>
     names: [...Object.values(defaultTopics)],
   });
 
+  const addFile = async (path: string, content: string) => {
+    return await createOrUpdateFileContents({
+      repo: appRepository.name,
+      path: path,
+      content: base64(content),
+      message: `add ${path.split('/').pop()}`
+    });
+
+  };
   let commit: FileCommit;
+  commit = await addFile('.gitignore', createGitignore());
+  commit = await addFile('README.md', createReadme(appSpec));
+  commit = await addFile('unity-app.yaml', yaml.dump(appSpec));
+  commit = await addFile('.github/workflows/make-stub.yaml', createMakeStubWorkflow());
 
-  commit = await createOrUpdateFileContents({
-    repo: appRepository.name,
-    path: '.gitignore',
-    content: base64(createGitignore()),
-    message: `add .gitignore`
-  });
-
-  commit = await createOrUpdateFileContents({
-    repo: appRepository.name,
-    path: 'README.md',
-    content: base64(createReadme(appSpec)),
-    message: `add README.md`
-  });
-
-  commit = await createOrUpdateFileContents({
-    repo: appRepository.name,
-    path: 'unity-app.yaml',
-    content: base64(yaml.dump(appSpec)),
-    message: `add unity-app.yaml`
-  });
-
+  // it is important after which commit branching takes place
   for (const defaultBranch of Object.values(defaultBranches)) {
     await createAReference({
       repo: appRepository.name,
@@ -61,6 +96,7 @@ export const createRepository = async (appSpec: AppSpec): Promise<Repository> =>
       sha: commit.commit.sha ?? ''
     });
   }
+
 
   if ('members' in appSpec) {
     for (const member of appSpec.members) {

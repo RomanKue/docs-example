53889f84b118cc34ca3d2cc908f2cb6a08a5c25d
Daniel Strobusch (FG-410) <Daniel.Strobusch@bmw.de>
2022-09-09 19:58:02 +0200

    improve workspaces

--- actions/src/lib/unity/app-repo/workflows/ci/ci-angular-workflow.ts
+++ actions/src/lib/unity/app-repo/workflows/ci/ci-angular-workflow.ts
@@ -12,42 +12,69 @@ env:
 jobs:
   ${ciAction}-${name}:
     runs-on: atc-ubuntu-latest
-    timeout-minutes: 30
+    timeout-minutes: 10
     permissions:
       contents: read
       id-token: write
       packages: write
     steps:
       - uses: actions/checkout@v3
+      - name: cache
+        id: cache
+        uses: actions/cache@v3
+        with:
+          # don't rebuild all the time if nothing relevant changes
+          key: ci-\${{ env.DEPLOYMENT }}-\${{ hashFiles( format('{0}/**', env.DEPLOYMENT ), '.github/workflows/**' ) }}
+          path: \${{ env.DEPLOYMENT }}
       - uses: actions/setup-node@v3
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
         with:
           node-version: "16"
           cache: npm
           cache-dependency-path: \${{ env.DEPLOYMENT }}/package-lock.json
       - name: ci
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
         working-directory: \${{ env.DEPLOYMENT }}
         run: npm ci
-      - name: test
-        working-directory: \${{ env.DEPLOYMENT }}
-        run: npx ng test
       - name: build
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
         working-directory: \${{ env.DEPLOYMENT }}
         run: npx ng build -c production
-      - name: build image
+      - name: install chromium
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
+        run: sudo apt install chromium-browser
+      - name: test
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
         working-directory: \${{ env.DEPLOYMENT }}
+        env:
+          CHROME_BIN: /usr/bin/chromium-browser
+        run: npx ng test --browsers=ChromeHeadless --watch=false
+      - name: docker login
+        if: \${{ steps.cache.outputs.cache-hit != 'true' && github.ref == 'refs/heads/main' }}
+        uses: docker/login-action@v2
+        with:
+          registry: containers.atc-github.azure.cloud.bmw
+          username: USERNAME
+          password: \${{ secrets.GITHUB_TOKEN }}
+      - name: docker config
         env:
             REGISTRY: ${containerRegistry}
             REGISTRY_PASSWORD: \${{ secrets.GITHUB_TOKEN }}
             IMAGE_GROUP: ${unityOrg.toLowerCase()}
             IMAGE_NAME: \${{ github.event.repository.name }}-\${{ env.DEPLOYMENT }}
             IMAGE_TAG: latest
+        run: echo "IMAGE=\${{ env.IMAGE_GROUP }}/\${{ env.IMAGE_NAME }}:\${{ env.IMAGE_TAG }}" >> $GITHUB_ENV
+      - name: build image
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
+        working-directory: \${{ env.DEPLOYMENT }}
         run: |
             echo "
             FROM nginxinc/nginx-unprivileged:latest
             LABEL org.opencontainers.image.source \${{ github.event.repository.html_url }}
             COPY dist/\${{ env.DEPLOYMENT }} /usr/share/nginx/html
-            " > docker build -t \${{ env.IMAGE_GROUP }}/\${{ env.IMAGE_NAME }}:\${{ env.IMAGE_TAG }} -
-            echo \${{ env.REGISTRY_PASSWORD }} | docker login \${{ env.REGISTRY }} -u USERNAME --password-stdin
-            docker push -t \${{ env.IMAGE_GROUP }}/\${{ env.IMAGE_NAME }}:\${{ env.IMAGE_TAG }}
+            " > docker build -t \${{ env.IMAGE }} -
+      - name: push image
+        if: \${{ steps.cache.outputs.cache-hit != 'true' && github.ref == 'refs/heads/main' }}
+        working-directory: \${{ env.DEPLOYMENT }}
+        run: docker push \${{ env.IMAGE }}
     `.trim();
-
--- actions/src/lib/unity/app-repo/workflows/ci/ci-quarkus-workflow.ts
+++ actions/src/lib/unity/app-repo/workflows/ci/ci-quarkus-workflow.ts
@@ -13,22 +13,32 @@ env:
 jobs:
   ${ciAction}-${name}:
     runs-on: atc-ubuntu-latest
-    timeout-minutes: 30
+    timeout-minutes: 10
     permissions:
       contents: read
       id-token: write
       packages: write
     steps:
       - uses: actions/checkout@v3
+      - name: cache
+        id: cache
+        uses: actions/cache@v3
+        with:
+          # don't rebuild all the time if nothing relevant changes
+          key: ci-\${{ env.DEPLOYMENT }}-\${{ hashFiles( format('{0}/**', env.DEPLOYMENT ), '.github/workflows/**' ) }}
+          path: \${{ env.DEPLOYMENT }}
       - name: setup java
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
         uses: actions/setup-java@v3
         with:
           distribution: "zulu"
           java-version: "17"
           cache: maven
       - name: setup maven
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
         uses: pmd/setup-maven@v1
       - name: build image
+        if: \${{ steps.cache.outputs.cache-hit != 'true' }}
         working-directory: \${{ env.DEPLOYMENT }}
         # see https://quarkus.io/guides/container-image#container-image-options
         env:
@@ -39,7 +49,7 @@ jobs:
           QUARKUS_CONTAINER_IMAGE_TAG: latest
           QUARKUS_CONTAINER_IMAGE_GROUP: ${unityOrg.toLowerCase()}
           QUARKUS_CONTAINER_IMAGE_BUILD: "true"
-          QUARKUS_CONTAINER_IMAGE_PUSH: "true"
+          QUARKUS_CONTAINER_IMAGE_PUSH: \${{ github.ref == 'refs/heads/main' }}
         run: mvn clean package
     `.trim();
 

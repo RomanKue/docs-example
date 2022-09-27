a03ad9b7a8c6fc36205036af6c9a06162b1b9428
Daniel Strobusch (FG-410) <Daniel.Strobusch@bmw.de>
2022-09-27 07:54:50 +0200

    fix linting issue

--- .github/actions/make-quarkus-stub/templates/GreetingResourceTest.java
+++ .github/actions/make-quarkus-stub/templates/GreetingResourceTest.java
@@ -12,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+            .when().get("/")
+            .then()
+               .statusCode(200)
+               .body(is("Hello from RESTEasy Reactive"));
     }
 
 }

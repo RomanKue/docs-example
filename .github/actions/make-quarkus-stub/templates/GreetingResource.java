d7858bba9889ffeaac2242e2baf35be72ed448d8
Daniel Strobusch (FG-410) <Daniel.Strobusch@bmw.de>
2022-11-10 15:12:19 +0100

    add docs on envoy and cert.

--- .github/actions/make-quarkus-stub/templates/GreetingResource.java
+++ .github/actions/make-quarkus-stub/templates/GreetingResource.java
@@ -1,16 +1,17 @@
 {{ .Env.PACKAGE }}
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/")
+@Path("/v1/")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public String hello(@HeaderParam("Unity-UserName") final String userName) {
+        return "Hello " + userName + ", this is UNITY 🚀";
     }
 }
--- docs/index.md
+++ docs/index.md
@@ -1,8 +1,9 @@
+<!-- DOCTOC SKIP -->
 # UNITY
 
 ![](assets/unity_logo.png)
 
-# Table of Contents
+## Table of Contents
 
 * [Terms of Service](Terms-of-Service.md)
 * [DevOps Handbook](dev-ops-handbook/index.md)

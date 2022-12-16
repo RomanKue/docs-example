cf609073332b43de3f9fb348b8c2604fc49f9d00
Gyorgyi Nagy (ext.) <Gyorgyi.Nagy@partner.bmw.de>
2022-12-16 09:31:14 +0100

    DMPDM-980 - Update according to code review

--- .github/actions/make-angular-stub/create-angular-app.bash
+++ .github/actions/make-angular-stub/create-angular-app.bash
@@ -39,7 +39,7 @@ ng new "$NAME" --defaults --style=scss --skip-git
 
   < "$SCRIPT_PATH/templates/app.module.ts" gomplate > "src/app/app.module.ts"
 
-  mkdir -p .src/app/core/interceptors
+  mkdir -p src/app/core/interceptors
 
   < "$SCRIPT_PATH/templates/authentication-error-interceptor.ts" gomplate > "src/app/core/interceptors/authentication-error-interceptor.ts"
 

632524956dc7ebcdb379cf3c18b859d9e25bc3fa
Marius Petoi (ext.) <Marius.Petoi@partner.bmw.de>
2023-01-23 14:59:23 +0200

    PSKD-57866: Fix lint

--- actions/src/actions/create-prod-k8s-objects.ts
+++ actions/src/actions/create-prod-k8s-objects.ts
@@ -1,5 +1,5 @@
-import {run} from "../lib/run.js";
-import {createK8sObjects} from "../lib/unity/app-repo/k8s.js";
+import {run} from '../lib/run.js';
+import {createK8sObjects} from '../lib/unity/app-repo/k8s.js';
 
 run(async () => {
   const appRepos = ['app-test', 'app-approval-plan', 'app-multi-pak', 'app-pmd-data-viewer', 'app-sa-bhe', 'app-lbac',

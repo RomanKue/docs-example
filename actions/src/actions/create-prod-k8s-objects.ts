c888c1b8211e6f845241cd0f02fb9433f864b4f8
Marius Petoi (ext.) <Marius.Petoi@partner.bmw.de>
2023-01-30 09:58:48 +0200

    PSKD-57866: Adapt applications

--- actions/src/actions/create-prod-k8s-objects.ts
+++ actions/src/actions/create-prod-k8s-objects.ts
@@ -2,7 +2,7 @@ import {run} from '../lib/run.js';
 import {createK8sObjects} from '../lib/unity/app-repo/k8s.js';
 
 run(async () => {
-  const appRepos = ['app-test', 'app-approval-plan', 'app-multi-pak', 'app-pmd-data-viewer', 'app-sa-bhe', 'app-lbac',
+  const appRepos = ['app-test', 'app-approval-plan', 'app-multi-pak', 'app-pmd-data-viewer', 'app-sa-bhe', 'app-rrps', 'app-foo',
     'app-services', 'app-ebr', 'app-blacklist-pendelschlag', 'app-pn-interpreter', 'app-fgp', 'app-rocketeers-app', 'app-newapp', 'app-landing-page'];
   await appRepos.forEach(async (repo) => {
     await createK8sObjects('prod', repo);

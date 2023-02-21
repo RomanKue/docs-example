c74f2f4fd2f703f4ccfa0fe33d3c772af1ac93d7
Attila Zsombori (ext.) <attila.zsombori@partner.bmw.de>
2023-02-21 16:40:26 +0200

    UNITYAPPS-134: Change isSecretExistent implementation

--- actions/src/lib/github/api/actions/actions-utils.ts
+++ actions/src/lib/github/api/actions/actions-utils.ts
@@ -1,20 +1,10 @@
-import {RequestError} from '@octokit/request-error';
-import {ActionsApi, getAnEnvironmentSecret} from './actions.js';
+import {listEnvironmentSecrets} from './actions.js';
 
 export const isSecretExistent = async (
   options: {
     repository_id: number,
     environment_name: string,
     secret_name: string,
-  } & Partial<Parameters<ActionsApi['getEnvironmentSecret']>[0]>
-) => {
-  try {
-    await getAnEnvironmentSecret(options);
-    return true;
-  } catch (e) {
-    if (e instanceof RequestError && e.status === 404) {
-      return false;
-    }
-    throw e;
-  }
+  }): Promise<boolean> => {
+  return (await listEnvironmentSecrets(options)).map(as => as.name).includes(options.secret_name);
 };

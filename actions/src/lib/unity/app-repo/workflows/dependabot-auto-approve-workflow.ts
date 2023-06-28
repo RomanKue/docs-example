2db53cabc105b6a06688821b221de2f9dbf5ceeb
Daniel Strobusch (FG-41) <Daniel.Strobusch@bmw.de>
2023-06-28 13:43:19 +0200

    improve dependabot setup
     * act on pull_request_target
     * enable auto merge
     * prepare for auto approval

--- actions/src/lib/unity/app-repo/workflows/dependabot-auto-approve-workflow.ts
+++ actions/src/lib/unity/app-repo/workflows/dependabot-auto-approve-workflow.ts
@@ -1,7 +1,7 @@
 export const dependabotAutoApproveWorkflowFileName = 'dependabot-auto-approve.yaml';
 export const createDependabotAutoApproveWorkflow = () => `
 name: dependabot-auto-approve
-on: pull_request
+on: pull_request_target
 
 permissions:
   id-token: write
@@ -23,10 +23,17 @@ jobs:
         shell: bash
         run: >
           echo '\${{ secrets.GITHUB_TOKEN }}' | gh auth login --hostname atc-github.azure.cloud.bmw --with-token
-      - name: Approve non-major version updates
-        if: \${{ steps.metadata.outputs.update-type == 'version-update:semver-patch' || steps.metadata.outputs.update-type == 'version-update:semver-minor' }}
-        run: gh pr review --approve "$PR_URL"
+      - name: Enable auto merge
+        run: gh pr merge --auto --merge "$PR_URL"
         env:
           PR_URL: \${{ github.event.pull_request.html_url }}
+          GITHUB_TOKEN: \${{ secrets.GITHUB_TOKEN }}
+      # the following step can merge minor and patch version upgrades automatically after a successful build
+      # - name: Approve non-major version updates
+      #   if: \${{ steps.metadata.outputs.update-type == 'version-update:semver-patch' || steps.metadata.outputs.update-type == 'version-update:semver-minor' }}
+      #   run: gh pr review --approve "$PR_URL"
+      #   env:
+      #     PR_URL: \${{ github.event.pull_request.html_url }}
+
     `.trim();
 
